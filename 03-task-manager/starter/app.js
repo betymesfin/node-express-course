2717b5143491b6e2704c97bd775ba11aa25d9f15
betymesfin <98676637+betymesfin@users.noreply.github.com>
2025-04-22 22:55:49 -0500

    week-6(Task manager part2)

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/task");
 require("dotenv").config();
-const port = 3000;
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("hurrray");
-});
-
+app.use(express.static("./public"));
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandler);
 
 const start = async () => {
   try {
