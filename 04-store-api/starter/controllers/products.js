cb4be5e52054cece9512c30f24bedb74117121de
betymesfin <98676637+betymesfin@users.noreply.github.com>
2025-05-04 17:11:45 -0500

    corrected some typo

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product");
 
-const getAllProductsStatic = async (req, res) => {
+const getAllProductStatic = async (req, res) => {
   const products = await Product.find({})
     .sort("name")
     .select("name price")
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 
   res.status(200).json({ products, nbHits: products.length });
 };
-const getAllProducts = async (req, res) => {
+const getAllProduct = async (req, res) => {
   const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
@@ -66,6 +66,6 @@ const getAllProducts = async (req, res) => {
 };
 
 module.exports = {
-  getAllProducts,
-  getAllProductsStatic,
+  getAllProduct,
+  getAllProductStatic,
 };
